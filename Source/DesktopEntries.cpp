921c43685fbea249cc0e68f3b34ba13c71b17208
centuryglass <anthony0857@gmail.com>
2017-12-15 18:27:34 -0600

    copied the wrong file

--- Source/DesktopEntries.cpp
+++ Source/DesktopEntries.cpp
@@ -6,7 +6,6 @@
     Author:  anthony
 
   ==============================================================================
-*/
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,33 +13,72 @@
  */
 
 /* 
- * File:   DesktopEntries.h
+ * File:   DesktopEntries.cpp
  * Author: anthony
- *
+ * 
  * Created on December 14, 2017, 1:36 PM
  */
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <stdlib.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include "DesktopEntries.h"
+#include "sysutils.h"
 
-#ifndef DESKTOPENTRIES_H
-#define DESKTOPENTRIES_H
+
+DesktopEntries::DesktopEntries() {
+    std::string localeName=getLocale();
+    //read the contents of all desktop application directories
+    std::vector<std::string> dirs={
+    "~/.local/share/applications",
+    "/usr/share/applications",
+    "/usr/local/share/applications"
+    };
+    std::vector<std::string> files {};
+    for(int i=0;i<dirs.size();i++){
+        std::vector<std::string> dfiles=listFiles(dirs[i]);
+        for(int i2=0;i2<dfiles.size();i2++){
+            files.push_back(dirs[i]+"/"+dfiles[i2]);
+        }
+    }
+    //read in files as DesktopEntry objects
+    std::regex dfileMatch(".*\\.(desktop|directory)$",std::regex::icase);
+    for(std::vector<std::string>::iterator it=files.begin();
+            it!= files.end();it++){
+        std::string path = *it;
+        if(std::regex_search(path,dfileMatch)){
+            entries.push_back(DesktopEntry(path,localeName));
+        }
+    }
+    std::cout<<"found "<<entries.size()<<" entries\n";
+}
+
+DesktopEntries::DesktopEntries(const DesktopEntries& orig) {
+    entries=orig.entries;
+}
+
+DesktopEntries::~DesktopEntries() {
+}
 
 
-#include <vector>
-#include <map>
-#include <string>
-#include "DesktopEntry.h"
+void DesktopEntries::printAll(){
+    for(int i=0;i<entries.size();i++){
+        std::cout<<"entry "<<i<<":"<<entries[i].getName()<<",exec="
+                <<entries[i].getExec()<<"\n";
+    }
+}
 
-class DesktopEntries {
-public:
-    DesktopEntries();
-    DesktopEntries(const DesktopEntries& orig);
-    virtual ~DesktopEntries();
-    void printAll();
-    int size();
-    DesktopEntry getEntry(int index);
-private:
-    std::vector<DesktopEntry> entries;
- 
-};
+int DesktopEntries::size(){
+    return entries.size();
+}
 
-#endif /* DESKTOPENTRIES_H */
+
+DesktopEntry DesktopEntries::getEntry(int index){
+    return entries[index];
+}
+
+
 
