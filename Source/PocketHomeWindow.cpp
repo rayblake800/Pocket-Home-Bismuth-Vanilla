3f84f8bb1b6c58ca8e79550a3573be4d78038160
centuryglass <anthony0857@gmail.com>
2018-03-10 22:25:11 -0600

    Wifi thread now pauses when the window loses focus

--- Source/PocketHomeWindow.cpp
+++ Source/PocketHomeWindow.cpp
@@ -5,6 +5,7 @@
 #include "WindowFocusedTimer.h"
 #include "Utils.h"
 #include "PocketHomeWindow.h"
+#include "PocketHomeApplication.h"
 
 PocketHomeWindow::PocketHomeWindow(String windowName) :
 DocumentWindow(windowName, Colours::darkgrey, DocumentWindow::allButtons),
@@ -43,12 +44,32 @@ loginPage([this] ()
 
 PocketHomeWindow::~PocketHomeWindow() { }
 
-void PocketHomeWindow::activeWindowStatusChanged() { 
-    if(isActiveWindow()){
+void PocketHomeWindow::activeWindowStatusChanged()
+{
+    WifiStatus& wifiStatus =
+            PocketHomeApplication::getInstance()->getWifiStatus();
+    if (isActiveWindow())
+    {
         WindowFocusedTimer::windowFocusGained();
+        if (wifiStatus.isEnabled())
+        {
+            Thread* wifiThread = dynamic_cast<Thread*> (&wifiStatus);
+            if (wifiThread != nullptr && !wifiThread->isThreadRunning())
+            {
+                DBG(__func__ << "starting wifi thread");
+                wifiThread->startThread();
+            }
+        }
     }
     else
     {
+        Thread* wifiThread = dynamic_cast<Thread*> (&wifiStatus);
+        if (wifiThread != nullptr && wifiThread->isThreadRunning())
+        {
+            DBG(__func__ << ":stopping wifi thread");
+            wifiThread->signalThreadShouldExit();
+            wifiThread->notify();
+        }
         WindowFocusedTimer::windowFocusLost();
     }
     pageStack.windowFocusChanged(isActiveWindow());
