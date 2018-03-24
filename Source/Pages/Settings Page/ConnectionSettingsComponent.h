36b593b55f9fe80d335772f5b40825edbcc88786
centuryglass <anthony0857@gmail.com>
2018-03-24 12:23:18 -0500

    updated connectionSettingsComponent documentation

--- Source/Pages/Settings Page/ConnectionSettingsComponent.h
+++ Source/Pages/Settings Page/ConnectionSettingsComponent.h
@@ -3,8 +3,8 @@
  * 
  * ConnectionSettingsComponent is a widget that provides a connection status
  * icon, a toggle switch for enabling/disabling the connection, and a button
- * that shows a connection status string, and opens a connection page when
- * clicked while the switch is enabled.
+ * that shows a connection status string. The button will also open a 
+ * connection page when clicked while the switch is enabled.
  */
 #pragma once
 
@@ -21,9 +21,11 @@ private Button::Listener
 {
 public:
     /**
-     * @param openConnectionPage callback to run when the button is clicked.
-     * This should be a function that opens a connection settings page.
-     * @param name internal component name.
+     * @param openConnectionPage  A callback to run when the button is clicked.
+     *                             This should be a function that opens a 
+     *                             connection settings page.
+     * 
+     * @param name                The internal component name.
      */
     ConnectionSettingsComponent(
             std::function<void() > openConnectionPage,
@@ -44,6 +46,7 @@ private:
      * Check the connections enabled status directly.  This will be used to
      * determine what position the switch should be in when the component
      * is created or refreshed.
+     * 
      * @return true if connections are enabled, false if disabled.
      */
     virtual bool connectionEnabled() = 0;
@@ -51,21 +54,23 @@ private:
     /**
      * This is used to determine when the component should show a loading
      * spinner and disable all input.
+     * 
      * @return true if connection status is busy, false if it's ready for
-     * input.
+     *          input.
      */
     virtual bool isBusy() = 0;
 
     /**
      * @return an appropriate asset file name for the connection type and
-     * status.
+     *          status.
      */
     virtual String getIconAsset() = 0;
 
     /**
      * This will be called whenever the enable/disable switch is flipped.
      * Subclasses should implement this to enable/disable their connections.
-     * @param enabled
+     * 
+     * @param enabled  Passes on the new switch state.
      */
     virtual void enabledStateChanged(bool enabled) = 0;
 
@@ -85,8 +90,9 @@ private:
 
     /**
      * If the connection button is clicked this will run openConnectionPage(). 
-     * If the switch is clicked, this will call  enabledStateChanged(), passing
+     * If the switch is clicked, this will call enabledStateChanged(), passing
      * it the switch toggle state.
+     * 
      * @param b
      */
     void buttonClicked(Button *b) override;
@@ -104,7 +110,9 @@ private:
     {
     public:
         /**
-         * @param name internal component name
+         * @param config  Shared UI preferences, needed for text rendering.
+         * 
+         * @param name    The internal component name
          */
         ConnectionButton(
                 ComponentConfigFile& config,
@@ -120,7 +128,8 @@ private:
 
     private:
         /**
-         * Draws the connection button outline and prints the button text
+         * Draws the connection button outline and prints the button text.
+         * 
          * @param g
          * @param isMouseOverButton
          * @param isButtonDown
@@ -132,18 +141,31 @@ private:
          * Calculates button text height based on button size.
          */
         void resized() override;
-
+        
+        //used for finding text height when the component is resized
         ComponentConfigFile& config;
+        //last calculated text height
         int textHeight;
+        //calculated when the component is resized, set to a tenth of component
+        //height
         int borderSize;
+        //Connection information text
         String displayText;
+        
         JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ConnectionButton)
     };
+    //Shared UI component preferences
     ComponentConfigFile& config;
+    //Icon indicating the connection type and/or status
     DrawableImageComponent icon;
+    //Shown when the connection is busy updating
     Spinner spinner;
+    //Switch to enable/disable the connection
     SwitchComponent toggle;
+    //Can be clicked to open the connection page
     ConnectionButton pageButton;
+    //Callback function that opens the connection page
     std::function<void() > openConnectionPage;
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ConnectionSettingsComponent)
 };
