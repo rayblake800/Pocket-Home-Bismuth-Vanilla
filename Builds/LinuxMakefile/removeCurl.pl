72ae028039ffa90c481b6825ddd82bcc08c55bc6
centuryglass <anthony0857@gmail.com>
2018-03-26 02:51:02 -0500

    added comments explaining removeCurl.pl

--- Builds/LinuxMakefile/removeCurl.pl
+++ Builds/LinuxMakefile/removeCurl.pl
@@ -1,6 +1,11 @@
 use strict;
 use warnings;
 
+# The Projucer is still the easiest way to make sure the makefiles generate
+# correctly, but it insists on linking libcurl even though that option is
+# disabled.  This will remove all instances of the string " libcurl" from the
+# makefile.
+
 open(my $makefile, "<", "Makefile") or die "Can't read from makefile!";
 
 my $file = "";
