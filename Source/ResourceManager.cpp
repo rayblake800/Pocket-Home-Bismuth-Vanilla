0a39462792133b60faba9734a6a6bea4cae72d04
centuryglass <anthony0857@gmail.com>
2018-03-29 12:10:37 -0500

    renamed RAIISingleton to ResourceManager

---
+++ Source/ResourceManager.cpp
@@ -0,0 +1,47 @@
+#include "ResourceManager.h"
+
+/**
+ * Throws an error if the resource is destroyed while the reference
+ * list is not empty.
+ */
+ResourceManager::SharedResource::~SharedResource()
+{
+    if (!referenceList.isEmpty())
+    {
+        DBG("~SharedResource Error: shared resource is being destroyed while "
+                << referenceList.size() << " references still exist!");
+    }
+}
+
+ResourceManager::ResourceManager(
+        ScopedPointer<SharedResource>& classResource,
+        CriticalSection& resourceLock,
+        std::function<SharedResource*() > createResource) :
+classResource(classResource),
+resourceLock(resourceLock)
+{
+    const ScopedLock initLock(resourceLock);
+    if (classResource == nullptr)
+    {
+        classResource = createResource();
+    }
+    classResource->referenceList.add(this);
+}
+
+/**
+ * This will remove itself from the sharedResource's reference list, and
+ * destroy the resource if this was the last reference in the list.
+ */
+ResourceManager::~ResourceManager()
+{
+    const ScopedLock initLock(resourceLock);
+    if (classResource == nullptr)
+    {
+        DBG("ResourceManager Error: shared resource is missing!");
+    }
+    classResource->referenceList.removeAllInstancesOf(this);
+    if (classResource->referenceList.isEmpty())
+    {
+        classResource = nullptr;
+    }
+}
