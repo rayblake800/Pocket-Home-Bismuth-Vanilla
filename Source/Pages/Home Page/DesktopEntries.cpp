d1f4529a4d22c17221cb59b0ad37906187667159
centuryglass <anthony0857@gmail.com>
2018-04-11 14:32:24 -0500

    replaced sleep() with yield()

--- Source/Pages/Home Page/DesktopEntries.cpp
+++ Source/Pages/Home Page/DesktopEntries.cpp
@@ -134,7 +134,7 @@ void DesktopEntries::run()
             {
                 return;
             }
-            wait(-1);
+            Thread::yield();
         }
         uiCallPending = true;
         MessageManager::callAsync([&i, &dirs, &uiCallPending, this]
@@ -170,7 +170,7 @@ void DesktopEntries::run()
             {
                 return;
             }
-            wait(-1);
+            Thread::yield();
         }
         uiCallPending = true;
         MessageManager::callAsync([&fileIndex, &files, &uiCallPending, this]
