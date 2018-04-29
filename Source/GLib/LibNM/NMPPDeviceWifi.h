249e8a599e14323e18de9685654e642ab483aa94
Anthony Brown <anthony0857@gmail.com>
2018-04-28 21:02:02 -0500

    started wifi device documentation

--- Source/GLib/LibNM/NMPPDeviceWifi.h
+++ Source/GLib/LibNM/NMPPDeviceWifi.h
@@ -4,64 +4,170 @@
 #include "NMPPConnection.h"
 #include "GPPObject.h"
 
+/**
+ * @file NMPPDeviceWifi.h
+ *
+ * @brief A RAII container and C++ interface for LibNM NMDeviceWifi
+ *        objects.
 class NMPPDeviceWifi : public GPPObject
 {
 public:
+    /**
+     * Creates a NMPPDeviceWifi object that shares a NMDeviceWifi*
+     * with another NMPPDeviceWifi object
+     *
+     * @param toCopy  This object's NMDeviceWifi* will be shared with the
+     *                new object.
+     */
     NMPPDeviceWifi(const NMPPDeviceWifi& toCopy);
     
+    /**
+     * 
+     * @param toAssign
+     */
     NMPPDeviceWifi(NMDeviceWifi* toAssign);
     
+    /**
+     *
+     */
     NMPPDeviceWifi();
     
+    /**
+     *
+     * @return
+     */
     NMDeviceState getState();
     
+    /**
+     * @return
+     */
     NMDeviceStateReason getStateReason();
     
+    /**
+     * Checks if NetworkManager manages this device.
+     *
+     * @return  true iff this device is managed by NetworkManager.
+     */
     bool isManaged();
     
+    /**
+     *
+     * @return
+     */
     const char* getInterface();
     
+    /**
+     *
+     */
     const char* getPath();
     
+    /**
+     *
+     */
     void disconnect();
     
+    /**
+     *
+     * @return
+     */
     NMPPConnection getActiveConnection();
     
+    /**
+     *
+     * @param path
+     *
+     * @return
+     */
     NMPPAccessPoint getAccessPoint(const char* path);
     
+    /**
+     *
+     * @return
+     */
     Array<NMPPAccessPoint> getAccessPoints();
     
+    /**
+     *
+     */
     class WifiListener : public GPPObject::SignalHandler
     {
     public:
         WifiListener() { }
         virtual ~WifiListener() { }
     private:
+	/**
+	 *
+	 * @param device
+	 *
+	 * @param newState
+	 *
+	 * @param oldState
+	 *
+	 * @param reason
+	 */
         virtual void stateChanged(NMPPDeviceWifi* device,
                 NMDeviceState newState,
                 NMDeviceState oldState,
                 NMDeviceStateReason reason) = 0;
         
+	/**
+	 * @param addedAP
+	 */
         virtual void accessPointAdded(NMAPAccessPoint addedAP) = 0;
         
+	/**
+	 * @param removedAP
+	 */
         virtual void accessPointRemoved(NMAPAccessPoint removedAP) = 0;
     };
     
+    /**
+     * @param listener
+     */
     void addListener(WifiListener* listener);
     
+    /**
+     * @param listener
+     */
     void removeListener(WifiListener* listener);
     
 private:
+    /**
+     * @param device
+     *
+     * @param newState
+     *
+     * @param oldState
+     *
+     * @param reason
+     *
+     * @param listener
+     */
     static void stateChangeCallback(
             NMDevice* device,
-	    NMDeviceState new_state,
-            NMDeviceState old_state,
+	    NMDeviceState newState,
+            NMDeviceState oldState,
             NMDeviceStateReason reason,
             WifiListener* listener);
     
+    /**
+     * @param device
+     *
+     * @param ap
+     *
+     * @param listener
+     */
     static void apAddedCallback(NMDeviceWifi* device, NMAccessPoint* ap,
             WifiListener* listener);
     
+    /**
+     * @param device
+     *
+     * @param ap
+     *
+     * @param listener
+     *
+     */
     static void apRemovedCallback(NMDeviceWifi* device, NMAccessPoint* ap,
             WifiListener* listener);
 };
