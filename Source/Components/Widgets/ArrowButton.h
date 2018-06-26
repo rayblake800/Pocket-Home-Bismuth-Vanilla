5dca0f093ad5d2dc98d9cdd03e007a7c60a8737b
centuryglass <anthony0857@gmail.com>
2018-06-26 14:30:54 -0500

    defining ArrowButton class

--- Source/Components/Widgets/ArrowButton.h
+++ Source/Components/Widgets/ArrowButton.h
@@ -1,11 +1,48 @@
-/*
-  ==============================================================================
-
-    ArrowButton.h
-    Created: 24 Jun 2018 11:55:50am
-    Author:  anthony
-
-  ==============================================================================
-*/
-
 #pragma once
+#include "ConfigurableImageButton"
+
+/**
+ * @file ArrowButton.h
+ * 
+ * @brief A directional navigation button assigned to one edge of the window.
+ *        Button position and direction are chosen based on the assigned edge.
+ */
+
+class ArrowButton : public ConfigurableImageButton
+{
+public:
+    /**
+     */
+    enum WindowEdge
+    {
+        up,
+        down,
+        left,
+        right
+    };
+    
+    /**
+     * @param side
+     */
+    ArrowButton(WindowEdge side);
+    
+    virtual ~ArrowButton();
+    
+    /**
+     * @return
+     */
+    float horizMarginFractionNeeded() const;
+    
+    /**
+     * @return
+     */
+    float vertMarginFractionNeeded() const;
+private:
+    /**
+     * @param side
+     * 
+     * @return
+     */
+    static String getComponentKey(ScreenSide side);
+    
+};
