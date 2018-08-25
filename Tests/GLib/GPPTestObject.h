33d53e6fd7fc5e635b7a3df4e277602238ef8547
centuryglass <anthony0857@gmail.com>
2018-08-25 14:31:11 -0500

    building tests to properly diagnose and solve glib problems

---
+++ Tests/GLib/GPPTestObject.h
@@ -0,0 +1,15 @@
+#pragma once
+
+//https://developer.gnome.org/gobject/stable/howto-gobject.html
+
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define TEST_OBJECT_TYPE gpp_test_object_get_type()
+G_DECLARE_FINAL_TYPE (TestObject, test_object, TEST, OBJECT, GObject)
+
+TestObject* test_object_new();
+
+G_END_DECLS
