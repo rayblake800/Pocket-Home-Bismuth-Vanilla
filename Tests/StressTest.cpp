3cb280c68dbb85622c49b545876e4e400787fe84
centuryglass <anthony0857@gmail.com>
2018-08-27 13:02:53 -0500

    implemented base StressTest class

--- Tests/StressTest.cpp
+++ Tests/StressTest.cpp
@@ -1,11 +1,90 @@
-/*
-  ==============================================================================
-
-    StressTest.cpp
-    Created: 26 Aug 2018 12:47:27pm
-    Author:  anthony
-
-  ==============================================================================
-*/
-
 #include "StressTest.h"
+
+/*
+ * Initialize a new StressTest.
+ */
+StressTest::StressTest(juce::String testName, int minThreads, int maxThreads,
+int actionFreq, int testDuration) :juce::UnitTest(testName),
+    minThreads(minThreads), maxThreads(maxThreads),
+    actionFreq(actionFreq), testDuration(testDuration)
+{
+    using namespace juce;
+    addAction([this]()
+            {
+                const ScopedLock threadLock(threads.getLock());
+                if(threads.size() < this->maxThreads)
+                {
+                    threads.add(new TestThread(*this));
+                    threads.getLast()->startThread();
+                }
+            });
+    
+    addAction([this]()
+            {
+                const ScopedLock threadLock(threads.getLock());
+                if(threads.size() > this->minThreads)
+                {
+                    int threadNum = getRandom().nextInt() % threads.size();
+                    threads[threadNum]->signalThreadShouldExit();
+                }
+            });
+}
+
+/*
+ * Adds a new action for threads to randomly perform while testing. 
+ */
+void StressTest::addAction(std::function<void()> testAction)
+{
+    testActions.add(testAction);
+}
+
+/*
+ * Starts the test.  This creates the minimum number of threads, and allows
+ * them to perform random actions for <testDuration> seconds.  This should
+ * be called once in the StressTest::runTest() method of StressTest unit tests.
+ */
+void StressTest::runThreads()
+{
+    using namespace juce;
+    endTime = Time::currentTimeMillis() + 1000 * testDuration;
+    const ScopedLock threadLock(threads.getLock());
+    while(threads.size() < minThreads)
+    {
+        threads.add(new TestThread(*this));
+        threads.getLast()->startThread();
+    }
+    const ScopedUnlock threadUnlock(threads.getLock());
+    while(!threads.isEmpty())
+    {
+        for(int i = 0; i < threads.size(); i++)
+        {
+            if(threads[i] == nullptr || !threads[i]->isThreadRunning())
+            {
+                threads.remove(i);
+                i--;
+            }
+        }
+    }
+}
+
+/*
+ * Initializes a new test thread
+ */
+StressTest::TestThread::TestThread(StressTest& testSource) :
+juce::Thread(testSource.getName() + " Thread"), testSource(testSource) { }
+
+/*
+ * Runs a random function from testSource.testActions, then sleeps for
+ * a random amount of time less than testSource.actionFreq millseconds.
+ */
+void StressTest::TestThread::run()
+{
+    using namespace juce;
+    while(!threadShouldExit() && Time::currentTimeMillis() < testSource.endTime)
+    {
+        testSource.testActions[testSource.getRandom().nextInt()
+                % testSource.testActions.size()]();
+        sleep(testSource.getRandom().nextInt() % testSource.actionFreq);
+    }
+
+}
--- Tests/StressTest.h
+++ Tests/StressTest.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "JuceHeader.h"
 /**
  * @file StressTest.h
@@ -7,27 +8,84 @@
  */
 
 
-class StressTest 
+class StressTest : public juce::UnitTest 
 {
 public:
-    StressTest(int minThreads, int maxThreads, int testDuration);
+    /**
+     * Initialize a new StressTest.
+     *
+     * @param testName      A name identifying this StressTest.
+     *
+     * @param minThreads    Minimum number of testing threads to run at once.
+     *
+     * @param maxThreads    Maximum number of testing threads to run at once.
+     *
+     * @param actionFreq    Longest amount of time (in milliseconds) that
+     *                      threads should sleep between actions.
+     *
+     * @param testDuration  Duration to run tests, in seconds.
+     */
+    StressTest(juce::String testName, int minThreads, int maxThreads,
+            int actionFreq, int testDuration);
             
+protected:
+    /**
+     * Adds a new action for threads to randomly perform while testing. 
+     *
+     * @param testAction  A function that threads may randomly select to run
+     *                    while testing.
+     */
+    void addAction(std::function<void()> testAction);
 
-    void addAction(std::function<void()> testAction;);
-
-    void runTest() override;
-
+    /**
+     * Starts the test.  This creates the minimum number of threads, and allows
+     * them to perform random actions for <testDuration> seconds.  This should
+     * be called once in the runTest() method of StressTest unit tests.
+     */
+    void runThreads();
     
 private:
-    juce::Array<std::function<void()> testActions;
-
+    /**
+     * Randomly selects and performs actions for the duration of a test. 
+     */
     class TestThread : public juce::Thread
     {
     public:
+        /**
+         * Initializes a new test thread
+         *
+         * @param testSource  The test object creating this thread.
+         */
         TestThread(StressTest& testSource);
 
-        virtual ~TestThread();
+        virtual ~TestThread() { }
+    private:
+        /**
+         * Runs a random function from testSource.testActions, then sleeps for
+         * a random amount of time less than testSource.actionFreq millseconds.
+         */
+        virtual void run() override;
+        
+        /**
+         * The UnitTest that created the thread, used to access actions and
+         * frequency.
+         */
+        StressTest& testSource;
     };
 
-
-}
+    //All actions that TestThreads may select while testing.
+    juce::Array<std::function<void()>> testActions;
+    //Minimum number of TestThreads to run at once.
+    const int minThreads;
+    //Maximum number of TestThreads to run at once.
+    const int maxThreads;
+    //Maximum amount of time for threads to sleep between actions, in 
+    //milliseconds.
+    const int actionFreq;
+    //Amount of time to allow TestThreads to act, in seconds.
+    const int testDuration;
+    //Saves the time an active test should end, in milliseconds since the epoch.
+    juce::uint32 endTime;
+    //Holds all active test threads.
+    juce::OwnedArray<TestThread, juce::CriticalSection> threads;
+};
