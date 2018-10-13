674f50863d2f42de5912a2386f2a26256b0cea63
Anthony Brown <anthony0857@gmail.com>
2018-10-13 12:18:23 -0500

    Finished defining Nullable template class

--- Source/Util/Nullable.h
+++ Source/Util/Nullable.h
@@ -6,7 +6,7 @@
  * @brief  A template for classes that may or may not hold data.
  *
  * Nullable object containers are intended to package abstract pointer types.
- * 
+ *
  * Each Nullable class has a specific data pointer type.  Nullable classes
  * copy the interface of their data object, forwarding all function calls to
  * their object data pointer.  For each copied interface method, the nullable
@@ -30,24 +30,44 @@ template<class PtrType>
 Nullable
 {
 protected:
-    Nullable(PtrType objectPtr) : objectPtr(objectPtr) { }
+    /**
+     * @brief  Creates a Nullable containing a data pointer.
+     *
+     * @param dataPtr  The object's data pointer.
+     */
+    Nullable(PtrType dataPtr) : dataPtr(dataPtr) { }
 
-    Nullable() : objectPtr(nullptr) { }
+    /**
+     * @brief  Creates a null object containing no data.
+     */
+    Nullable() : dataPtr(nullptr) { }
 
 public:
     virtual ~Nullable() { }
 
+    /**
+     * @brief  Checks if this object's data pointer is null.
+     *
+     * @return  True if the data pointer equals nullptr, false if it points to
+     *          a data object.
+     */
     bool isNull() const
     {
-        return objectPtr == nullptr;
+        return dataPtr == nullptr;
     }
 
 protected:
-    PtrType& getObject()
+    /**
+     * @brief  Accesses the object's data pointer.
+     *
+     * @return  A reference to the internal data pointer.
+     */
+    PtrType& getData()
     {
-        return objectPtr;
+        return dataPtr;
     }
 
 private:
-    PtrType objectPtr;
+    /* Holds a pointer to object data that might be null. */
+    PtrType dataPtr;
 };
