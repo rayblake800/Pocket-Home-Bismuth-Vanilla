89f4429a7b6462aed8a90eb6d35489625298fea9
centuryglass <anthony0857@gmail.com>
2018-11-01 18:04:02 -0500

    Initial (untested) paged menu implementation

--- Source/AppMenu/Formats/Paged/MenuComponent.cpp
+++ Source/AppMenu/Formats/Paged/MenuComponent.cpp
@@ -1,28 +1,74 @@
-#if 0
+#define APPMENU_IMPLEMENTATION_ONLY
+#include "ComponentConfigKeys.h"
+#include "AppMenu/Data/ConfigKeys.h"
+#include "AppMenu/Formats/Paged/FolderComponent.h"
 #include "AppMenu/Formats/Paged/MenuComponent.h"
 
-#include "AppMenu/MenuComponents/MenuComponent.h"
- AppMenu::Paged::MenuComponent::~MenuComponent() { }/*
- * Arranges folders from top to bottom, with the active folder
- *         filling the menu's bounds.
+/* Animation duration in milliseconds: */
+static const constexpr int animationMS = 300;
+
+AppMenu::Paged::MenuComponent::MenuComponent() :
+ConfigurableComponent(ComponentConfigKeys::pagedAppMenuKey) 
+{ 
+    addTrackedKey(ConfigKeys::pagedMenuColumnsKey);
+    addTrackedKey(ConfigKeys::pagedMenuRowsKey);
+}
+ 
+/*
+ * Finds the bounds where a menu folder should be placed.
  */
-void AppMenu::Paged::MenuComponent::updateMenuLayout() final
+juce::Rectangle<int> AppMenu::Paged::MenuComponent::getFolderBounds
+(const int folderIndex, const bool closingFolder)
 {
-}/*
- * Creates a new Paged::FolderComponent for an opened folder
- *         menu item.
- */AppMenu::FolderComponent* AppMenu::Paged::MenuComponent::createFolderComponent(MenuItem folderItem)
-const final
-{
+    int openFolders = openFolderCount();
+    if(closingFolder)
+    {
+        openFolders--;
+    }
+    FolderComponent* folder = static_cast<AppMenu::Paged::FolderComponent*>
+        (getOpenFolder(folderIndex));
+    const int folderX = folder->getCurrentFolderPage() * -getWidth(); 
+    const int folderY = (folderIndex - openFolders + 1) * getHeight(); 
+    const int width   = getWidth() * folder->getNumFolderPages();
+    if(folder->getBounds().isEmpty() && folderIndex > 0)
+    {
+        // New folder opened, set initial bounds for nicer animation
+        folder->setBounds(folderX, folderY - getHeight(), width, getHeight());
+    }
+    return juce::Rectangle<int>(folderX, folderY, width, getHeight());
 }
 
 /*
- * Updates the menu's bounds whenever its parent component is
- *         resized.
- */void AppMenu::Paged::MenuComponent::parentResized(const juce::Rectangle<int> parentBounds)
-final
+ * Gets the duration in milliseconds to animate folder transitions.
+ */
+int AppMenu::Paged::MenuComponent::getAnimationDuration() const
 {
+    return animationMS;
 }
 
-/* Only include this file directly in the AppMenu implementation! */
-#endif
+/*
+ * Creates a new Paged::FolderComponent for an opened folder menu item.
+ */
+AppMenu::FolderComponent* 
+AppMenu::Paged::MenuComponent::createFolderComponent(MenuItem folderItem) const 
+{
+    return new AppMenu::Paged::FolderComponent(folderItem);
+}
+
+/*
+ * Updates the menu's bounds whenever its parent component is resized.
+ */
+void AppMenu::Paged::MenuComponent::parentResized
+(const juce::Rectangle<int> parentBounds)
+{
+    applyConfigBounds();
+}
+
+/*
+ * Updates the menu layout when the paged menu's grid dimensions change.
+ */
+void AppMenu::Paged::MenuComponent::configValueChanged
+(const juce::Identifier& propertyKey)
+{
+    updateMenuLayout(false);
+}
--- Source/AppMenu/Formats/Paged/MenuComponent.h
+++ Source/AppMenu/Formats/Paged/MenuComponent.h
@@ -2,6 +2,8 @@
 #ifdef APPMENU_IMPLEMENTATION_ONLY
 
 #pragma once
+#include "ConfigurableComponent.h"
+#include "AppMenu/Data/ConfigFile.h"
 #include "AppMenu/Components/MenuComponent.h"
 
 /**
@@ -10,21 +12,36 @@
  * @brief  Creates and arranges application menu folder components for the
  *         paged AppMenu format.
  */
-class AppMenu::Paged::MenuComponent : public AppMenu::MenuComponent
+class AppMenu::Paged::MenuComponent : public AppMenu::MenuComponent,
+    public ConfigurableComponent, public ConfigFile::Listener
 {
 public:
-    MenuComponent() { }
-
+    MenuComponent();
+    
     virtual ~MenuComponent() { }
 
-protected:
-    /**
-     * @brief  Arranges folders from top to bottom, with the active folder
-     *         filling the menu's bounds.
-     */
-    virtual void updateMenuLayout() final override;
-
 private:
+    /**
+     * @brief   Finds the bounds where a menu folder should be placed.
+     *
+     * @param folderIndex    The index of an open folder component.
+     *
+     * @param closingFolder  Whether the active folder (not necessarily this
+     *                       folder!) is about to be closed.
+     *
+     * @return               The bounds within the MenuComponent where the
+     *                       folder should be placed.
+     */
+     virtual juce::Rectangle<int> getFolderBounds(const int folderIndex,
+            const bool closingFolder = false) final override;
+
+    /**
+     * @brief  Gets the duration in milliseconds to animate folder transitions.
+     *
+     * @return   The AppMenu format's animation duration.
+     */
+    virtual int getAnimationDuration() const final override;
+
     /**
      * @brief  Creates a new Paged::FolderComponent for an opened folder
      *         menu item.
@@ -43,6 +60,15 @@ private:
      * @param parentBounds  The new bounds of the parent component.
      */
     virtual void parentResized(const juce::Rectangle<int> parentBounds)
+        final override;
+
+    /**
+     * @brief  Updates the menu layout when the paged menu's grid dimensions 
+     *         change.
+     *
+     * @param propertyKey  The paged menu's row or column count key.
+     */
+    virtual void configValueChanged(const juce::Identifier& propertyKey)
         final override;
 };
 
