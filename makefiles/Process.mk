5d41d428a1d31975440dbcd85e4e7df827ea2b2a
Anthony Brown <anthony0857@gmail.com>
2018-11-19 20:55:00 -0600

    Fixed makefile directory error

--- makefiles/Process.mk
+++ makefiles/Process.mk
@@ -4,12 +4,12 @@ PROCESS_ROOT = Source/Process
 PROCESS_TEST_ROOT = Tests/Process
 
 OBJECTS_PROCESS := \
-  $(CONFIG_PREFIX)Reader.o \
-  $(CONFIG_PREFIX)Launched.o \
-  $(CONFIG_PREFIX)Launcher.o  
+  $(PROCESS_PREFIX)Reader.o \
+  $(PROCESS_PREFIX)Launched.o \
+  $(PROCESS_PREFIX)Launcher.o  
 
 OBJECTS_PROCESS_TEST := \
-  $(CONFIG_PREFIX)Test_Launched.o
+  $(PROCESS_PREFIX)Test_Launched.o
 
 ifeq ($(BUILD_TESTS), 1)
     OBJECTS_PROCESS := $(OBJECTS_PROCESS) $(OBJECTS_PROCESS_TEST)
@@ -22,15 +22,15 @@ OBJECTS_APP := $(OBJECTS_APP) $(OBJECTS_PROCESS)
 process : $(OBJECTS_PROCESS)
 	@echo "Built Process module"
 
-$(CONFIG_PREFIX)Reader.o : \
+$(PROCESS_PREFIX)Reader.o : \
     $(PROCESS_ROOT)/ProcessUtils.cpp
-$(CONFIG_PREFIX)Launched.o : \
+$(PROCESS_PREFIX)Launched.o : \
     $(PROCESS_ROOT)/LaunchedProcess.cpp
-$(CONFIG_PREFIX)Launcher.o : \
+$(PROCESS_PREFIX)Launcher.o : \
     $(PROCESS_ROOT)/AppLauncher.cpp
-$(CONFIG_PREFIX)Launched.o : \
+$(PROCESS_PREFIX)Launched.o : \
     $(PROCESS_ROOT)/LaunchedProcess.cpp
 
 # Tests:
-$(CONFIG_PREFIX)Test_Launched.o : \
+$(PROCESS_PREFIX)Test_Launched.o : \
     $(PROCESS_TEST_ROOT)/LaunchedProcess.cpp
