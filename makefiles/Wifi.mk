098cfea71d391fb86b12faa3d381254163c26b59
Anthony Brown <anthony0857@gmail.com>
2018-12-03 15:31:29 -0600

    Updated Wifi makefile, renamed Wifi icon

--- makefiles/Wifi.mk
+++ makefiles/Wifi.mk
@@ -3,19 +3,31 @@ WIFI_PREFIX := $(JUCE_OBJDIR)/Wifi_
 WIFI_ROOT = Source/Wifi
 WIFI_TEST_ROOT = Tests/Wifi
 
-OBJECTS_WIFI := \
-  $(WIFI_PREFIX)APHash.o \
-  $(WIFI_PREFIX)StateManager.o \
-  $(WIFI_PREFIX)AccessPoint.o \
-  $(WIFI_PREFIX)NetworkInterface.o \
-  $(WIFI_PREFIX)LibNMInterface.o \
-  $(WIFI_PREFIX)JsonInterface.o \
-  $(WIFI_PREFIX)SettingsComponent.o \
-  $(WIFI_PREFIX)SettingsPage.o \
-  $(WIFI_PREFIX)Icon.o
+SAVED_STATE_ROOT := $(WIFI_ROOT)/SavedState
+OBJECTS_WIFI_SAVED_STATE := \
+  $(WIFI_PREFIX)DeviceTracker.o
 
-OBJECTS_WIFI_TEST := \
-  $(WIFI_PREFIX)Test_StateManager.o
+CONTROL_ROOT := $(WIFI_ROOT)/Control
+OBJECTS_WIFI_CONTROL := \
+  $(WIFI_PREFIX)Controller.o
+
+LISTENER_ROOT := $(WIFI_ROOT)/Listeners
+OBJECTS_WIFI_LISTENER := \
+  $(WIFI_PREFIX)DeviceListener.o
+
+COMPONENT_ROOT := $(WIFI_ROOT)/Component
+OBJECTS_WIFI_COMPONENT := \
+  $(WIFI_PREFIX)ControlWidget.o \
+  $(WIFI_PREFIX)StatusIcon.o \
+  $(WIFI_PREFIX)SettingsPage.o
+
+OBJECTS_WIFI := \
+  $(OBJECTS_WIFI_SAVED_STATE) \
+  $(OBJECTS_WIFI_CONTROL) \
+  $(OBJECTS_WIFI_LISTENER) \
+  $(OBJECTS_WIFI_COMPONENT)
+
+OBJECTS_WIFI_TEST :=
 
 ifeq ($(BUILD_TESTS), 1)
     OBJECTS_WIFI := $(OBJECTS_WIFI) $(OBJECTS_WIFI_TEST)
@@ -28,25 +40,18 @@ OBJECTS_APP := $(OBJECTS_APP) $(OBJECTS_WIFI)
 wifi : $(OBJECTS_WIFI)
 	@echo "Built Wifi module"
 
-$(WIFI_PREFIX)APHash.o : \
-    $(WIFI_ROOT)/APHash.cpp
-$(WIFI_PREFIX)StateManager.o : \
-    $(WIFI_ROOT)/WifiStateManager.cpp
-$(WIFI_PREFIX)AccessPoint.o : \
-    $(WIFI_ROOT)/WifiAccessPoint.cpp
-$(WIFI_PREFIX)NetworkInterface.o : \
-    $(WIFI_ROOT)/NetworkInterface.cpp
-$(WIFI_PREFIX)LibNMInterface.o : \
-    $(WIFI_ROOT)/LibNMInterface.cpp
-$(WIFI_PREFIX)JsonInterface.o : \
-    $(WIFI_ROOT)/JsonWifiInterface.cpp
-$(WIFI_PREFIX)SettingsComponent.o : \
-    $(WIFI_ROOT)/WifiSettingsComponent.cpp
-$(WIFI_PREFIX)SettingsPage.o : \
-    $(WIFI_ROOT)/WifiSettingsPage.cpp
-$(WIFI_PREFIX)Icon.o : \
-    $(WIFI_ROOT)/WifiIcon.cpp
+$(WIFI_PREFIX)DeviceTracker.o : \
+    $(SAVED_STATE_ROOT)/DeviceTracker.cpp
 
-# Tests:
-$(WIFI_PREFIX)Test_StateManager.o : \
-    $(WIFI_TEST_ROOT)/WifiStateManager.cpp
+$(WIFI_PREFIX)Controller.o : \
+    $(CONTROL_ROOT)/Controller.cpp
+
+$(WIFI_PREFIX)DeviceListener.o : \
+    $(LISTENER_ROOT)/DeviceListener.cpp
+
+$(WIFI_PREFIX)ControlWidget.o : \
+    $(COMPONENT_ROOT)/ControlWidget.cpp
+$(WIFI_PREFIX)StatusIcon.o : \
+    $(COMPONENT_ROOT)/StatusIcon.cpp
+$(WIFI_PREFIX)SettingsPage.o : \
+    $(COMPONENT_ROOT)/SettingsPage.cpp
