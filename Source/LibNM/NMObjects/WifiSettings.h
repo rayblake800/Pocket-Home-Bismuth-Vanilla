0d5f2b83b924948ccb09fc5fbb2863fff0ef6034
Anthony Brown <anthony0857@gmail.com>
2018-12-16 21:34:01 -0600

    Documenting new connection setting objects.

--- Source/LibNM/NMObjects/WifiSettings.h
+++ Source/LibNM/NMObjects/WifiSettings.h
@@ -11,27 +11,87 @@
 
 namespace LibNM { class WifiSettings; }
 namespace LibNM { class SSID; }
+namespace LibNM { enum class APMode; }
 
 class LibNM::WifiSettings : public Settings
 {
 public:
+    /**
+     * @brief  Creates a WifiSettings object that shares data with another
+     *         WifiSettings object.
+     *
+     * @param toCopy  The WifiSettings object that will share its LibNM data
+     *                with the new object.
+     */
     WifiSettings(const WifiSettings& toCopy);
 
+    /**
+     * @brief  Creates a WifiSettings object to hold existing wireless settings
+     *         data.
+     *
+     * @param toAssign  The LibNM wireless settings object that the new 
+     *                  WifiSettings object will hold.
+     */
     WifiSettings(NMSettingWireless* toAssign);
 
+    /**
+     * @brief  Creates a new WifiSettings object holding a new, empty LibNM
+     *         wireless settings object.
+     */
     WifiSettings();
 
+    /**
+     * @brief  Gets the SSID saved to the wireless connection settings.
+     *
+     * @return  The main identifier for this WifiSettings object's 
+     *          connection.
+     */
     SSID getSSID() const;
 
-    NM80211Mode getMode() const;
+    /**
+     * @brief  Gets the access point mode saved to the wireless connection
+     *         settings.
+     *
+     * @return  The access point mode for this WifiSettings object's
+     *          connection.
+     */
+    APMode getMode() const;
 
+    /**
+     * @brief  Gets the list of all base ID values associated with this
+     *         WifiSettings object's connection.
+     *
+     * @return  The list of all access point Mac addresses known to be used
+     *          by this object's connection.
+     */
     juce::StringArray getSeenBSSIDs() const;
 
+    /**
+     * @brief  Checks if this object's connection is hidden.
+     *
+     * @return  Whether the object's connection is a hidden Wifi connection. 
+     */
     bool isHidden() const;
 
+    /**
+     * @brief  Changes the SSID saved to tbe Wifi settings.
+     *
+     * @param newSSID  The new connection ID value to save to Wifi settings.
+     */
     void setSSID(const SSID newSSID);
 
-    void setMode(const NM80211Mode newMode);
+    /**
+     * @brief  Changes the access point mode saved to the Wifi settings.
+     *
+     * @param newMode  The new access point mode to set.
+     */
+    void setMode(const APMode newMode);
 
+    /**
+     * @brief  Sets if the connection is hidden.
+     *
+     * @param isHidden  Whether this object's connection should be marked as
+     *                  hidden.
+     */
     void setHidden(const bool isHidden);
 };
