63c884bc2bad8b72e07eed4055dcedadc409a6a4
centuryglass <anthony0857@gmail.com>
2019-01-03 14:09:44 -0600

    fixed minor typo

--- project-scripts/gdbLocal.sh
+++ project-scripts/gdbLocal.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Launch pocket-home with gdb, using local libraries compiled with debug flags.
-env LD_LIBARARY_PATH=/home/$USER/.local/lib:$LD_LIBARARY_PATH gdb pocket-home
+env LD_LIBRARY_PATH=/home/$USER/.local/lib:$LD_LIBARARY_PATH gdb pocket-home
