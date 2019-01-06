f5d59ed508b05b31a62815c5923d44fd186d3863
centuryglass <anthony0857@gmail.com>
2019-01-06 13:38:53 -0600

    Removed overlooked duplicate directory

--- Source/FreeDesktop/DesktopEntry/UpdateListener.cpp
+++
@@ -1,6 +0,0 @@
-#define DESKTOP_ENTRY_IMPLEMENTATION_ONLY
-#include "DesktopEntry/UpdateListener.h"
-#include "DesktopEntry/LoadingThread.h"
-
-DesktopEntry::UpdateListener::UpdateListener() : 
-    SharedResource::Handler<LoadingThread>() { }
