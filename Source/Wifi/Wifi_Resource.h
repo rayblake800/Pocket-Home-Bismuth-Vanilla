6b73c49536216d3850caf37ea7946e1948c7daff
centuryglass <anthony0857@gmail.com>
2019-02-07 00:39:57 -0600

    Explicitly defining template function specializations, otherwise its not guaranteed that they'll be found

--- Source/Wifi/Wifi_Resource.h
+++ Source/Wifi/Wifi_Resource.h
@@ -28,3 +28,65 @@ public:
      */
     virtual ~Resource();
 };
+
+/* Resource module class declarations: */
+namespace Wifi
+{
+    namespace LibNM { namespace Thread { class Module; } }
+    namespace Device { class Module; }
+    namespace APList { class Module; }
+    namespace Connection
+    {
+        namespace Saved { class Module; }
+        namespace Record { class Module; }
+        namespace Control { class Module; }
+    }
+}
+
+/* getModule() template specializations: */
+namespace SharedResource { namespace Modular {
+
+template<> template<> Wifi::LibNM::Thread::Module*
+SharedResource::Modular::Resource<>::getModule<Wifi::LibNM::Thread::Module>();
+
+template<> template<> Wifi::Device::Module* 
+SharedResource::Modular::Resource<>::getModule<Wifi::Device::Module>();
+
+template<> template<> Wifi::Connection::Saved::Module* 
+SharedResource::Modular::Resource<>::getModule
+        <Wifi::Connection::Saved::Module>();
+
+template<> template<> Wifi::Connection::Record::Module* 
+SharedResource::Modular::Resource<>::getModule
+        <Wifi::Connection::Record::Module>();
+
+template<> template<> Wifi::Connection::Control::Module* 
+SharedResource::Modular::Resource<>::getModule
+        <Wifi::Connection::Control::Module>();
+
+template<> template<> Wifi::APList::Module* 
+SharedResource::Modular::Resource<>::getModule <Wifi::APList::Module>();
+
+template<> template<> const Wifi::LibNM::Thread::Module*
+SharedResource::Modular::Resource<>::getModule
+        <const Wifi::LibNM::Thread::Module>();
+
+template<> template<> const Wifi::Device::Module* 
+SharedResource::Modular::Resource<>::getModule <const Wifi::Device::Module>();
+
+template<> template<> const Wifi::Connection::Saved::Module* 
+SharedResource::Modular::Resource<>::getModule
+        <const Wifi::Connection::Saved::Module>();
+
+template<> template<> const Wifi::Connection::Record::Module* 
+SharedResource::Modular::Resource<>::getModule
+        <const Wifi::Connection::Record::Module>();
+
+template<> template<> const Wifi::Connection::Control::Module* 
+SharedResource::Modular::Resource<>::getModule
+        <const Wifi::Connection::Control::Module>();
+
+template<> template<> const Wifi::APList::Module* 
+SharedResource::Modular::Resource<>::getModule<const Wifi::APList::Module>();
+
+} } // Close SharedResource::Modular namespace
