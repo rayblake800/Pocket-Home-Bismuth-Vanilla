40be7a1ee09776b5f6db4e53fbe7bacb9aef1e3f
centuryglass <anthony0857@gmail.com>
2019-02-27 00:06:17 -0600

    Removed empty Bluetooth module files

--- Source/Bluetooth/Bluetooth_SettingsPage.cpp
+++
@@ -1,1 +0,0 @@
-#include "Bluetooth_SettingsPage.h"
--- makefiles/Bluetooth.mk
+++ makefiles/Bluetooth.mk
@@ -3,11 +3,7 @@ BLUETOOTH_PREFIX := $(JUCE_OBJDIR)/Bluetooth_
 BLUETOOTH_ROOT = Source/Bluetooth
 
 OBJECTS_BLUETOOTH := \
-  $(BLUETOOTH_PREFIX)BluezAdapter.o \
-  $(BLUETOOTH_PREFIX)Status.o \
-  $(BLUETOOTH_PREFIX)Device.o \
-  $(BLUETOOTH_PREFIX)SettingsComponent.o \
-  $(BLUETOOTH_PREFIX)SettingsPage.o 
+  $(BLUETOOTH_PREFIX)BluezAdapter.o 
 
 OBJECTS_BLUETOOTH_TEST :=
 
@@ -24,11 +20,3 @@ bluetooth : $(OBJECTS_BLUETOOTH)
 
 $(BLUETOOTH_PREFIX)BluezAdapter.o : \
     $(BLUETOOTH_ROOT)/Bluetooth_BluezAdapter.cpp
-$(BLUETOOTH_PREFIX)Status.o : \
-    $(BLUETOOTH_ROOT)/Bluetooth_Status.cpp
-$(BLUETOOTH_PREFIX)Device.o : \
-    $(BLUETOOTH_ROOT)/Bluetooth_Device.cpp
-$(BLUETOOTH_PREFIX)SettingsComponent.o : \
-    $(BLUETOOTH_ROOT)/Bluetooth_SettingsComponent.cpp
-$(BLUETOOTH_PREFIX)SettingsPage.o : \
-    $(BLUETOOTH_ROOT)/Bluetooth_SettingsPage.cpp
