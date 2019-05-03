37af1857c0eb9af32fa3d0f8a8f473eb38e6225e
centuryglass <anthony0857@gmail.com>
2019-05-03 01:19:24 -0500

    Added scripts to get battery info that should work on most systems

---
+++ assets/scripts/batteryCheck.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Checks if a battery is available
+#
+# Prints the battery voltage file or device if a battery is found.
+
+# Check for PocketCHIP battery voltage file:
+voltagePath="/usr/lib/pocketchip-batt/voltage"
+if [ -f "$voltagePath" ]; then
+    echo "$voltagePath"
+fi
+
+# Check for battery devices through upower:
+if [ ! -z `command -v upower`]; then
+    batteryDevice=`upower -e | grep -i -m 1 'BAT'`
+    if [ ! -z "$batteryDevice" ]; then
+        echo "$batteryDevice"
+        exit 0
+    fi
+fi
+
+# Check for battery devices through acpi:
+if [ ! -z `command -v acpi` ]; then
+    batteryDevice=`acpi -b`
+    if [ ! -z "$batteryDevice" ]; then
+        echo "$batteryDevice"
+        exit 0
+    fi
+fi
+
+# Check for battery directories in /sys/class/power_supply:
+batteryDir=`ls /sys/class/power_supply`
+if [ ! -z "$batteryDir" ]
+    echo "$batteryDir"
+    exit 0
+fi
+
+# Check for battery directories in /proc/acpi/battery (deprecated):
+batteryDir=`ls /proc/acpi/battery`
+if [ ! -z "$batteryDir" ]
+    echo "$batteryDir"
+    exit 0
+fi
