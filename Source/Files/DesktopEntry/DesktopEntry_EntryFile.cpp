dde6f3828ada3f96ab2af03dd246ddb319a532cd
centuryglass <anthony0857@gmail.com>
2019-05-07 19:35:39 -0500

    Improved application-specific desktop entry filtering.
    Desktop entry files where OnlyShowIn contains "pocket-home" will now be
    displayed, and files where NotShowIn contains "pocket-home" will be hidden.

--- Source/Files/DesktopEntry/DesktopEntry_EntryFile.cpp
+++ Source/Files/DesktopEntry/DesktopEntry_EntryFile.cpp
@@ -328,7 +328,7 @@ juce::String DesktopEntry::EntryFile::getGenericName() const
 // Checks if this desktop entry should appear in application menus.
 bool DesktopEntry::EntryFile::shouldBeDisplayed() const
 {
-    return !noDisplay 
+    return !noDisplay
         && (onlyShowIn.isEmpty() || onlyShowIn.contains("pocket-home"))
         && ! notShowIn.contains("pocket-home");
 }
